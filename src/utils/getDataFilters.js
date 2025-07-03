f2d33e864f5deb14bc55e7460c6a5b1950c1782c
QuesoParmesano <pecazon03@gmail.com>
2025-07-02 21:25:31 -0300

    .

--- src/utils/getDataFilters.js
+++ src/utils/getDataFilters.js
@@ -1,7 +1,8 @@
+const AlumPerfiles = require('../database/models/AlumPerfiles.js')
 const Asistencias = require('../database/models/Asistencias.js')
 const Anio = require('../database/models/Anio.js')
 
-async function getCursosFilter(anio){
+async function getCursos(anio) {
 
     let data = {
         cursos: []
@@ -14,10 +15,33 @@ async function getCursosFilter(anio){
         attributes: ['cursos']
     })
 
-    for( let i = 0; i < cursosData.length; i++){
+    for (let i = 0; i < cursosData.length; i++) {
         data.cursos.push(cursosData[i].dataValues.cursos)
     }
 
     return data
 
 }
+
+async function getAlumnosNames(idUser) {
+
+    let data = {
+        nombres: [],
+        apellidos: []
+    }
+
+    let alumnosData = await AlumPerfiles.findAll({
+        attributes: ['nombres', 'apellidos'],
+        where: {
+            idAlumno: idUser
+        }
+    })
+
+    for (let i = 0; i < alumnosData.length; i++) {
+        data.nombres.push(alumnosData[i].dataValues.nombres)
+        data.apellidos.push(alumnosData[i].dataValues.apellidos)
+    }
+
+}
+
+module.exports = { getCursos, getAlumnosNames }
